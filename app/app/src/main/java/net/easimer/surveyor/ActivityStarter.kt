96024b83ab6d88f649a64c8828de0d22922d945c
Daniel Meszaros <easimer@gmail.com>
2021-01-05 14:47:45 +0100

    Document ActivityStarter

--- app/app/src/main/java/net/easimer/surveyor/ActivityStarter.kt
+++ app/app/src/main/java/net/easimer/surveyor/ActivityStarter.kt
@@ -1,6 +1,19 @@
 package net.easimer.surveyor
 
+/**
+ * An interface through which components may start specific types of activities.
+ */
 interface ActivityStarter {
+    /**
+     * Starts a [MapActivity] activity that replays a specific recording.
+     * @param recId Identifier of the recording to be played back.
+     */
     fun startMapActivity(recId: Long)
+
+    /**
+     * Starts a [MapActivity] activity for a new recording.
+     * 
+     * This will in turn start a new [RecorderService] service.
+     */
     fun startMapActivity()
 }
