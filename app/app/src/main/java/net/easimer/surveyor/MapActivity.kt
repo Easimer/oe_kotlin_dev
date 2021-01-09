929d095d6a489cf790f245f3da0e3007ca60fa4c
Daniel Meszaros <easimer@gmail.com>
2021-01-09 12:47:20 +0100

    Fix crash when rotating the screen while the stats dialog is open

--- app/app/src/main/java/net/easimer/surveyor/MapActivity.kt
+++ app/app/src/main/java/net/easimer/surveyor/MapActivity.kt
@@ -1,12 +1,16 @@
 package net.easimer.surveyor
 
 import android.Manifest
+import android.app.Application
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.LinearLayout
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import net.easimer.surveyor.data.Location
 import net.easimer.surveyor.data.disk.RecordingRoomRepository
@@ -39,10 +43,6 @@ class MapActivity : PermissionCheckedActivity(), LocationUpdateObserver {
     private var recId: Long? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_map)
-        val mapContainer = findViewById<LinearLayout>(R.id.map_container)
-
         assert(intent != null && intent.extras != null)
         val startService = intent!!.extras!!.run {
             val kind = getInt(KIND)
@@ -59,10 +59,17 @@ class MapActivity : PermissionCheckedActivity(), LocationUpdateObserver {
 
             intent!!.extras!!.let {
                 val recID = it.getLong(REC_ID)
+                supportFragmentManager.fragmentFactory = FragmentFactory(application, this, this, recID)
                 this.recId = recID
                 MapTrackpointSourceFactory.make(this, repo, recID)
             }
         }
+
+        // supportFragmentManager.fragmentFactory must be set before calling super.onCreate, so we
+        // defer that until now
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+        val mapContainer = findViewById<LinearLayout>(R.id.map_container)
 
         val btnMarkPOI = findViewById<FloatingActionButton>(R.id.mark_point_of_interest)
         if (trackPtSrc.canMarkPointOfInterest()) {
@@ -172,5 +179,22 @@ class MapActivity : PermissionCheckedActivity(), LocationUpdateObserver {
             val dlg = StatisticsDialog(this, mdl)
             dlg.show(supportFragmentManager, "STATSDLG")
         } ?: throw IllegalStateException()
+    }
+
+    protected class FragmentFactory(
+        private val application: Application,
+        private val ctx: Context,
+        private val lifecycleOwner: LifecycleOwner,
+        private val recId: Long): androidx.fragment.app.FragmentFactory() {
+        override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
+            return when(className) {
+                StatisticsDialog::class.java.name -> {
+                    val repo = RecordingRoomRepository(application)
+                    val mdl = StatisticsDialogModel(lifecycleOwner, recId, repo)
+                    StatisticsDialog(ctx, mdl)
+                }
+                else -> super.instantiate(classLoader, className)
+            }
+        }
     }
 }
