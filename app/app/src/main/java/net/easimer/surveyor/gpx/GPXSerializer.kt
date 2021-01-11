9fe0022970887c7eb62a7e15989ef68560f1775b
Daniel Meszaros <easimer@gmail.com>
2021-01-11 09:14:50 +0100

    Make emptyValidation test pass
    - Introduce GPXSerializer::makeElement
    - Put elements <name> and <time> under <metadata>
    - Add creator attr to the <gpx> element
    - Format dates correctly

--- app/app/src/main/java/net/easimer/surveyor/gpx/GPXSerializer.kt
+++ app/app/src/main/java/net/easimer/surveyor/gpx/GPXSerializer.kt
@@ -7,10 +7,14 @@ import org.jdom2.Namespace
 import org.jdom2.output.Format
 import org.jdom2.output.XMLOutputter
 import java.io.Writer
+import java.text.SimpleDateFormat
 
 class GPXSerializer(
     private val adapter: IRecordingGPXAdapter
 ) {
+    private val ns = Namespace.getNamespace("g", "http://www.topografix.com/GPX/1/1")
+    private val dateFmt = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX")
+
     fun serialize(output: Writer) {
         val doc = makeDocument()
 
@@ -21,27 +25,38 @@ class GPXSerializer(
 
     private fun makeDocument(): Document {
         val result = Document()
-        val root = Element("gpx")
-        root.namespace = Namespace.getNamespace("g", "http://www.topografix.com/GPX/1/1")
+        val root = makeElement("gpx")
         root.run {
             attributes.add(Attribute("version", "1.1"))
+            attributes.add(Attribute("creator", "http://github.com/easimer/oe_kotlin_dev/"))
         }
 
         addMetadata(root)
-
         result.addContent(root)
 
         return result
     }
 
     private fun addMetadata(root: Element) {
-        Element("name").run {
-            addContent(adapter.name)
-            root.addContent(this)
+        makeElement("metadata").let { metadata ->
+            makeElement("name").run {
+                addContent(adapter.name)
+                metadata.addContent(this)
+            }
+            makeElement("time").run {
+                addContent(dateFmt.format(adapter.time))
+                metadata.addContent(this)
+            }
+            root.addContent(metadata)
         }
-        Element("time").run {
-            addContent(adapter.time.toString())
-            root.addContent(this)
-        }
+    }
+
+    /**
+     * Make an XML element in the GPX namespace.
+     */
+    private fun makeElement(name: String): Element {
+        val ret = Element(name)
+        ret.namespace = ns
+        return ret
     }
 }
